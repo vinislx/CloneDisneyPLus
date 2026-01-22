bf53fbe26362c64887bfdfb874924a910e8e0a23
vinislx <vini91911@gmail.com>
2026-01-22 19:04:42 -0300

    alteração de posição de pasta images para o src

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 const Gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function compileSass() {
     return Gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(Gulp.dest('./dist/styles'));
-        
 }
 
-exports.default = compileSass;
+function comprimirimg() {
+    return Gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(Gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel (compileSass, comprimirimg);
 exports.watch = function() {
     Gulp.watch('./src/styles/*.scss', Gulp.parallel(compileSass));
 }
+
