491d126d0661dab39b6b9b4c186b72de4dc864ba
vinislx <vini91911@gmail.com>
2026-01-23 10:45:26 -0300

    adição de alteração do sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
-const Gulp = require('gulp');
+const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function compileSass() {
-    return Gulp.src('./src/styles/*.scss')
+    return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(Gulp.dest('./dist/styles'));
-        
+        .pipe(gulp.dest('./dist/styles'));
 }
 
-exports.default = compileSass;
+function comprimirimg() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel (compileSass, comprimirimg);
+
+
 exports.watch = function() {
-    Gulp.watch('./src/styles/*.scss', Gulp.parallel(compileSass));
+    gulp.watch('./src/styles/*.scss', gulp.parallel(compileSass));
 }
