7931bfeb93c60e4dc71664b9adc856181628b603
vinislx <vini91911@gmail.com>
2026-01-22 21:00:40 -0300

    ajustes no na escrita do gulp, estava escrita com letra maiuscula

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-const Gulp = require('gulp');
+const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 
 function compileSass() {
-    return Gulp.src('./src/styles/*.scss')
+    return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(Gulp.dest('./dist/styles'));
+        .pipe(gulp.dest('./dist/styles'));
 }
 
 function comprimirimg() {
-    return Gulp.src('./src/images/**/*')
+    return gulp.src('./src/images/**/*')
         .pipe(imagemin())
-        .pipe(Gulp.dest('./dist/images'));
+        .pipe(gulp.dest('./dist/images'));
 }
 
 exports.default = gulp.parallel (compileSass, comprimirimg);
 exports.watch = function() {
-    Gulp.watch('./src/styles/*.scss', Gulp.parallel(compileSass));
+    gulp.watch('./src/styles/*.scss', gulp.parallel(compileSass));
 }
 
